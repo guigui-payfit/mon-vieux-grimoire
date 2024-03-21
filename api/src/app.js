4d45a8ffc02a9262e5736d28a0d431d774e810e5
Guillaume CORNU <guillaume@payfit.com>
2024-03-21 15:15:41 +0100

    refactor(api): move book routes into a dedicated file

--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 
-const Book = require("./models/book");
+const bookRoutes = require("./routes/book");
 
 const app = express();
 app.use(express.json());
@@ -33,54 +33,6 @@ app.use((_, res, next) => {
   next();
 });
 
-app.delete("/api/books/:id", async (req, res, _) => {
-  try {
-    await Book.deleteOne({ _id: req.params.id });
-    res.status(200).json({ message: "Livre supprimé !" });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
-
-app.get("/api/books", async (_, res, __) => {
-  try {
-    const books = await Book.find();
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
-
-app.get("/api/books/:id", async (req, res, _) => {
-  try {
-    const book = await Book.findOne({ _id: req.params.id });
-    res.status(200).json(book);
-  } catch (error) {
-    res.status(404).json({ error });
-  }
-});
-
-app.post("/api/books", async (req, res, _) => {
-  try {
-    delete req.body._id;
-    const bookToCreate = { ...req.body, averageRating: 0, ratings: [] };
-    await new Book(bookToCreate).save();
-    res.status(201).json({ message: "Nouveau livre enregistré !" });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
-
-app.put("/api/books/:id", async (req, res, _) => {
-  try {
-    await Book.updateOne(
-      { _id: req.params.id },
-      { ...req.body, _id: req.params.id }
-    );
-    res.status(200).json({ message: "Livre modifié !" });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
+app.use("/api/books", bookRoutes);
 
 module.exports = app;
